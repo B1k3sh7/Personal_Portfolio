3dcd9332dc1b5c47bff60b8b931891970c53a344
BIkesh Shrestha <73869417+Bikesh2058@users.noreply.github.com>
2022-11-16 20:40:12 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,106 @@
+// Nav
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+// Nav for Mobile
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction() {
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// SKILLS
+const skillsContent = document.getElementsByClassName('skills_content');
+const skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_content skills_close';
+    }
+
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open';
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
+
+
+// Scroll Section
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 58;
+        const sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+// Scroll Up
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scroll >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+
+// Change Background Header
+function scrollHeader() {
+    const nav = doument.getElementById('header');
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+// Dark Light Theme
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selecte-icon', getCurrentIcon())
+})
+
 
